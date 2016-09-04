4e2290836a715ceb571c36678a6d81d2e4a00f4a
shresthaankit7 <shresthaankit7@gmail.com>
2016-09-05 00:26:48 +0545

    Log4j implementation demo

--- src/main/java/com/ankit/controller/MessageController.java
+++ src/main/java/com/ankit/controller/MessageController.java
@@ -1,9 +1,11 @@
 package com.ankit.controller;
 
 import com.ankit.domain.Message;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Created by ankit07 on 8/28/16.
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MessageController {
 
+    private static final Logger log = Logger.getLogger(MessageController.class);
+
     @RequestMapping("/hello/{player}")
     public Message message(@PathVariable String player){
         Message mgs = new Message(player,"HELLO " + player);
+        log.info("-- Send Hello message to " + player);
+
         return mgs;
     }
 }
